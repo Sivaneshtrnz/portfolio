8864164bc75148f8a521743e9170e9943ea6e63e
Sivanesh <74087015+Sivaneshtrnz@users.noreply.github.com>
2025-06-14 20:33:09 +0530

    Update script.js
    designation change

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Data Science Enthusiast","Backend-developer", "Freelancer"],
+        strings: [ "AI Professional","Backend-developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "Data Science Enthusiast","Backend-developer",  "Freelancer"],
+        strings: [ "AI Professional","Backend-developer",  "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
